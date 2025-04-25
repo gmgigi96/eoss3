cc14b4664bcd593e2a6de3e81a529ce1360f3d37
Gianmaria Del Monte <gianmaria.del.monte@cern.ch>
2025-04-25 14:57:35 +0200

    updated with latest version of plugin backend

--- plugin.go
+++ plugin.go
@@ -1,19 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"os"
 
 	"github.com/mitchellh/mapstructure"
 	"github.com/versity/versitygw/backend"
 	"github.com/versity/versitygw/plugins"
+	yaml "sigs.k8s.io/yaml/goyaml.v3"
 )
 
 var Backend plugins.BackendPlugin = plugin{}
 
 type plugin struct{}
 
-func (plugin) New(m map[string]any) (backend.Backend, error) {
-	fmt.Println(m)
+func (plugin) New(config string) (backend.Backend, error) {
+	f, err := os.Open(config)
+	if err != nil {
+		return nil, err
+	}
+
+	var m map[string]any
+	if err := yaml.NewDecoder(f).Decode(&m); err != nil {
+		return nil, err
+	}
+
 	var cfg Config
 	if err := mapstructure.Decode(m, &cfg); err != nil {
 		return nil, err
