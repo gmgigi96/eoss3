3b5f37d279a60a5d66daeda0dd2910fc1ed6720b
Gianmaria Del Monte <gianmaria.del.monte@cern.ch>
2025-04-28 17:56:02 +0200

    introduced registry to store bucket info

---
+++ registry/local.go
@@ -0,0 +1,131 @@
+package registry
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+type LocalRegistry struct {
+	base string
+}
+
+const (
+	bucketsFolder = "buckets"
+	usersFolder   = "users"
+)
+
+func NewLocalRegistry(folder string) (*LocalRegistry, error) {
+	r := LocalRegistry{
+		base: folder,
+	}
+	r.init()
+
+	return nil, nil
+}
+
+func (r *LocalRegistry) init() error {
+	bucketsPath := filepath.Join(r.base, bucketsFolder)
+	if err := os.MkdirAll(bucketsPath, 0755); err != nil {
+		return err
+	}
+	usersPath := filepath.Join(r.base, usersFolder)
+	if err := os.MkdirAll(usersPath, 0755); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *LocalRegistry) CreateMapping(mapping Mapping) error {
+	if _, err := r.GetMapping(mapping.Bucket); err == nil {
+		return ErrMappingAlreadyExisting
+	}
+
+	data, err := json.Marshal(mapping)
+	if err != nil {
+		return err
+	}
+
+	mappingPath := filepath.Join(r.base, bucketsFolder, mapping.Bucket)
+	return os.WriteFile(mappingPath, data, 0644)
+}
+
+func (r *LocalRegistry) GetMapping(bucket string) (Mapping, error) {
+	mappingPath := filepath.Join(r.base, bucketsFolder, bucket)
+	data, err := os.ReadFile(mappingPath)
+	if err != nil {
+		return Mapping{}, ErrNoSuchBucket
+	}
+	var m Mapping
+	if err := json.Unmarshal(data, &m); err != nil {
+		return Mapping{}, err
+	}
+	return m, nil
+}
+
+func (r *LocalRegistry) DeleteMapping(bucket string) error {
+	mappingPath := filepath.Join(r.base, bucketsFolder, bucket)
+	_ = os.Remove(mappingPath)
+	return nil
+}
+
+func (r *LocalRegistry) ListMappings() ([]Mapping, error) {
+	mappingPath := filepath.Join(r.base, bucketsFolder)
+	entries, err := os.ReadDir(mappingPath)
+	if err != nil {
+		return nil, err
+	}
+
+	list := make([]Mapping, 0, len(entries))
+	for _, entry := range entries {
+		bucketInfoPath := filepath.Join(mappingPath, entry.Name())
+		d, err := os.ReadFile(bucketInfoPath)
+		if err != nil {
+			continue
+		}
+		var m Mapping
+		if err := json.Unmarshal(d, &m); err != nil {
+			continue
+		}
+		list = append(list, m)
+	}
+	return list, nil
+}
+
+func (r *LocalRegistry) AssignBucket(bucket string, uid int) error {
+	uidStr := strconv.FormatInt(int64(uid), 10)
+	userBucketsPath := filepath.Join(r.base, usersFolder, uidStr)
+	if err := os.MkdirAll(userBucketsPath, 0755); err != nil {
+		return err
+	}
+	p := filepath.Join(userBucketsPath, bucket)
+	f, err := os.OpenFile(p, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return nil
+}
+
+func (r *LocalRegistry) ListBuckets(uid int) ([]string, error) {
+	uidStr := strconv.FormatInt(int64(uid), 10)
+	userBucketsPath := filepath.Join(r.base, usersFolder, uidStr)
+	entries, err := os.ReadDir(userBucketsPath)
+	if err != nil {
+		return []string{}, nil
+	}
+
+	list := make([]string, 0, len(entries))
+	for _, entry := range entries {
+		list = append(list, entry.Name())
+	}
+	return list, nil
+}
+
+func (r *LocalRegistry) UnassignBucket(bucket string, uid int) error {
+	uidStr := strconv.FormatInt(int64(uid), 10)
+	bucketPath := filepath.Join(r.base, usersFolder, uidStr, bucket)
+	_ = os.Remove(bucketPath)
+	return nil
+}
---
+++ registry/registry.go
@@ -0,0 +1,35 @@
+package registry
+
+import (
+	"errors"
+	"time"
+)
+
+// Mapping holds the information for mapping a bucket
+// with the real path on EOS.
+type Mapping struct {
+	// Bucket is the name of the bucket.
+	Bucket string
+	// Path is the real path on EOS.
+	Path string
+	// CreatedAt is the creation time of the bucket.
+	// Might be different from the actualt ctime of
+	// the corresponding folder in EOS.
+	CreatedAt time.Time
+}
+
+type Registry interface {
+	CreateMapping(mapping Mapping) error
+	GetMapping(bucket string) (Mapping, error)
+	DeleteMapping(bucket string) error
+	ListMappings() ([]Mapping, error)
+
+	AssignBucket(bucket string, uid int) error
+	ListBuckets(uid int) ([]string, error)
+	UnassignBucket(bucket string, uid int) error
+}
+
+var (
+	ErrMappingAlreadyExisting = errors.New("mapping already existing")
+	ErrNoSuchBucket           = errors.New("no such bucket")
+)
