96474df02a15a7e4eb0661380538bceeeeb703d7
Gianmaria Del Monte <g.macmount@gmail.com>
2025-07-17 17:31:40 +0200

    CLI: Add purge-bucket to cleanup the content of the EOS folder assigned to the bucket

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/user"
@@ -8,6 +9,7 @@ import (
 	"strings"
 	"time"
 
+	go_eosgrpc "github.com/cern-eos/go-eosgrpc"
 	"github.com/gmgigi96/eoss3/eos"
 	"github.com/gmgigi96/eoss3/meta"
 	"github.com/mitchellh/mapstructure"
@@ -39,6 +41,8 @@ func init() {
 
 	rootCmd.AddCommand(setDefaultPathCmd)
 	rootCmd.AddCommand(getDefaultPathCmd)
+	rootCmd.AddCommand(getBucketCmd)
+	rootCmd.AddCommand(purgeBucketCmd)
 }
 
 type Config struct {
@@ -153,6 +157,115 @@ var createBucketCmd = &cobra.Command{
 			_ = buckets.DeleteBucket(bucket.Name)
 			return err
 		}
+		return nil
+	},
+}
+
+var getBucketCmd = &cobra.Command{
+	Use:     "get-bucket <bucket>",
+	PreRunE: cobra.ExactArgs(1),
+	Short:   "Get the info of a bucket",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cfg, err := getConfig()
+		if err != nil {
+			return err
+		}
+
+		buckets, err := meta.New(cfg.Buckets)
+		if err != nil {
+			return err
+		}
+
+		bucketName := strings.TrimSpace(args[0])
+
+		b, err := buckets.GetBucket(bucketName)
+		if err != nil {
+			return err
+		}
+
+		return json.NewEncoder(os.Stdout).Encode(b)
+	},
+}
+
+func daemonEOSAuth() (eos.Auth, error) {
+	u, err := user.Lookup("daemon")
+	if err != nil {
+		return eos.Auth{}, err
+	}
+
+	uid, gid, err := getUidGid(u)
+	if err != nil {
+		return eos.Auth{}, err
+	}
+
+	return eos.Auth{
+		Uid: uid,
+		Gid: gid,
+	}, nil
+}
+
+var purgeBucketCmd = &cobra.Command{
+	Use:     "purge-bucket <bucket>",
+	PreRunE: cobra.ExactArgs(1),
+	Short:   "Erase the content of the bucket",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cfg, err := getConfig()
+		if err != nil {
+			return err
+		}
+
+		buckets, err := meta.New(cfg.Buckets)
+		if err != nil {
+			return err
+		}
+
+		bucketName := strings.TrimSpace(args[0])
+
+		b, err := buckets.GetBucket(bucketName)
+		if err != nil {
+			return err
+		}
+
+		client, err := eos.NewClient(eos.Config{
+			GrpcURL: cfg.GrpcURL,
+			HttpURL: cfg.HttpURL,
+			AuthKey: cfg.AuthKey,
+		})
+		if err != nil {
+			return err
+		}
+
+		nobody, err := daemonEOSAuth()
+		if err != nil {
+			return err
+		}
+
+		stat, err := client.Stat(cmd.Context(), nobody, b.Path)
+		if err != nil {
+			return fmt.Errorf("Error statting %s: %w", b.Path, err)
+		}
+
+		if stat.Cmd == nil {
+			return fmt.Errorf("%s does not exist or is not a directory", b.Path)
+		}
+
+		owner := eos.Auth{Uid: stat.Cmd.Uid, Gid: stat.Cmd.Gid}
+
+		if err := client.ListDir(cmd.Context(), nobody, b.Path, func(m *go_eosgrpc.MDResponse) {
+			var path string
+			if m.Cmd != nil {
+				path = string(m.Cmd.Path)
+			} else {
+				path = string(m.Fmd.Path)
+			}
+
+			if err := client.Remove(cmd.Context(), owner, path, true); err != nil {
+				fmt.Printf("Error removing %s: %v\n", path, err)
+			}
+		}, nil); err != nil {
+			return err
+		}
+
 		return nil
 	},
 }
